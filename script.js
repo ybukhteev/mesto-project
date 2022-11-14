1fd24e9a2241e6e181f4b519048718f6d0221480
ybukhteev <mmm424@yandex.ru>
2022-11-14 21:23:30 +0300

    add trash-icon

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
-const editPopupButton = document.querySelector('.profile__name-edit');
-const popup = document.querySelector('.popup');
+const profileEditBtn = document.querySelector('.profile__name-edit');  //  нашел кнопку редактирования профиля 
+const profilePopup = document.querySelector('.popup__edit-profile');  //  нашел попап редактирования профиля
+const cardAddBtn = document.querySelector('.profile__add-btn');  //  нашел кнопку добавления профиля
+const cardPopup = document.querySelector('.popup__add-card');  //  нашел окно для добавления карточки 
 
-const closePopupButton = document.querySelector('.popup__close');
+const profileName = document.querySelector('.profile__name'); //  нашел поле ввода имени
+const profileStatus = document.querySelector('.profile__status');  // нашел поле ввода профессии
 
-const profileName = document.querySelector('.profile__name');
-const profileStatus = document.querySelector('.profile__status');
-
+//  функция открытия попапа
 function openPopup(popup) {
   popup.classList.add('popup_opened');
 }
 
-editPopupButton.addEventListener('click', function () {
-  openPopup(popup);
+//  добавил слушатель событий на кнопку редактирования профиля, передал в него 2 пармаетра - клик и коллбэк
+profileEditBtn.addEventListener('click', function () {
+  openPopup(profilePopup);
 });
 
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-}
-
-closePopupButton.addEventListener('click', function () {
-  closePopup(popup);
+//  добавил слушатель событий на кнопку добавления профиля, переда в него 2 параметра - клик и коллбэк
+cardAddBtn.addEventListener('click', function () {
+  openPopup(cardPopup);
 });
 
-// Находим форму в DOM
-const formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
+const closePopupButton = document.querySelectorAll('.popup__close');  // нашел кнопку закрытия попапа
+
+// запустил 
+closePopupButton.forEach((popup) => {
+  popup.addEventListener('click', function (evt) {
+    const openModals = evt.target.closest('.popup');
+    openModals.classList.remove('popup_opened');
+  })
+})
+
+
+const formElement = document.querySelector('.popup__form'); //
 // Находим поля формы в DOM
-const nameInput = popup.querySelector('#username'); // Воспользуйтесь инструментом .querySelector()
-const jobInput = popup.querySelector('#status');  // Воспользуйтесь инструментом .querySelector()
+let nameInput = document.querySelector('#username');
+let jobInput = document.querySelector('#status');
+
+
+//  функция сохранения данных в форму профиля
+formElement.addEventListener('submit', handleFormSubmit);
 
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-
   profileName.textContent = nameInput.value;
   profileStatus.textContent = jobInput.value;
-  closePopup(popup);
-}
-
+  evt.target.closest('.popup').classList.remove('popup_opened');
+}   
