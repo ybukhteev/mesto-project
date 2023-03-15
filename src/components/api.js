d3061ddbd188412d014c81d17412929838e857b7
ybukhteev <mmm424@yandex.ru>
2023-03-15 10:18:51 +0300

    создать api.js и добавить объект для fetch запросов

---
+++ src/components/api.js
@@ -0,0 +1,11 @@
+// создал объект config в котром указал URL и заголовки для fetch запросов
+const config = {
+  baseUrl: 'https://nomoreparties.co/v1/plus-cohort-22',
+  header: {
+    authorization: '8f3ed13c-ae6e-4a59-b837-9d4380da0d56',
+    'Content-Type': 'application/json'
+  }
+}
+
+
+
