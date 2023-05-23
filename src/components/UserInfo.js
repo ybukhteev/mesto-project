8e3f146efb86c7d08f1257e105ebd630a64e9633
ybukhteev <mmm424@yandex.ru>
2023-05-23 19:16:23 +0300

    added class UserInfo

---
+++ src/components/UserInfo.js
@@ -0,0 +1,35 @@
+export default class UserInfo {
+  constructor({ profileName, profileStatus, profileAvatar }) {
+    this.profileName = profileName;
+    this.profileStatus = profileStatus;
+    this.profileAvatar = profileAvatar;
+  }
+
+  getUserInfo() {
+    return {
+      name: this.name,
+      about: this.about,
+      avatar: this.avatar,
+      _id: this._id
+    }
+  }
+
+  _updateAvatar() {
+    this.profileAvatar.src = this.avatar;
+  }
+
+  _updateUserInfo() {
+    this.profileName.textContent = this.name;
+    this.profileStatus.textContent = thus.about;
+  }
+
+  setUserInfo({ name, about, avatar, _id }) {
+    this.name = name;
+    this.about = about;
+    this.avatar = avatar;
+    this._id = _id;
+
+    this._updateUserInfo();
+    this._updateAvatar();
+  }
+}
